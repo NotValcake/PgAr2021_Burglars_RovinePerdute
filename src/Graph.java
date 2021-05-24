76c4334108c763ef8a00ad9ba86463671c84ebbf
NotValcake <r.valtorta@studenti.unibs.it>
2021-05-24 19:27:49 +0200

    added exception

--- src/Graph.java
+++ src/Graph.java
@@ -6,19 +6,47 @@ public class Graph {
         this.adjmat = new int[v][v];
     }
 
+    /**
+     * Gets the number of nodes in the graph
+     *
+     * @return number of nodes
+     */
     public int getVertexNum() {
         return adjmat.length;
     }
 
+    /**
+     * Gets the requested node with all of his edges
+     *
+     * @param v index of the wanted node
+     * @return the wanted node with all of his edges
+     */
     public int[] getVertex(int v) {
         return adjmat[v];
     }
 
+    /**
+     * Adds an Edge between the two specified nodes
+     *
+     * @param from starting node index
+     * @param to   destination node index
+     */
     public void addEdge(int from, int to) {
         adjmat[from][to] = 1;
     }
 
+    /**
+     * gets the value of the edge between the two specified nodes
+     *
+     * @param from starting node index
+     * @param to   destination node index
+     * @return 1 if the Edge exists
+     * @throws IllegalArgumentException if the same node is given as starting and dastination
+     */
     public int getEdge(int from, int to) {
+        if (from == to) {
+            throw (new IllegalArgumentException());
+        }
         return adjmat[from][to];
     }
 }
